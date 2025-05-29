a6b48d557525f554d7327e1adc9b6d1a7752ad46
wjy <1299454972@qq.com>
2025-05-29 11:24:33 +0800

    1.洛谷P1601,P1303

---
+++ LuoGu/P1303.cpp
@@ -0,0 +1,60 @@
+//
+// Created by Administrator on 2025/5/29.
+//
+#include "iostream"
+#include "string"
+#include "algorithm"
+#define maxn 4001
+using namespace std;
+int a[maxn],b[maxn],c[maxn];    // a存位数更长的那个
+int main(){
+    string A,B;
+    cin >> A >> B;
+    int len = max(A.length(),B.length());
+    int lessLength = -1;
+    int moreLength = -1;
+    // 反过来获取了
+    // 用更长的放在上面
+    if(A.length()>B.length()){
+        lessLength = B.length();
+        moreLength = A.length();
+        for(int i = A.length()-1,j=1;i>=0;i--,j++){
+            a[j] = A[i]-'0';
+        }
+        for(int i = B.length()-1,j=1;i>=0;i--,j++){
+            b[j] = B[i]-'0';
+        }
+    }else{
+        lessLength = A.length();
+        moreLength = B.length();
+        // b和a都从1位开始存,是和输入的是反过来的
+        for(int i = A.length()-1,j=1;i>=0;i--,j++){
+            b[j] = A[i]-'0';
+        }
+        for(int i = B.length()-1,j=1;i>=0;i--,j++){
+            a[j] = B[i]-'0';
+        }
+    }
+//    cout << a << ";" << b;
+
+    for(int j=1;j<=lessLength;j++){
+        for(int i=1;i<=moreLength;i++){
+            c[i] += b[j]*a[i];
+            c[i+1] = c[i]/10;
+            c[i] = c[i]%10;
+        }
+    }
+    // 笨一点的就是往后面找短的位，有出现不是0的，则给长度加上去
+    int CLength = moreLength;
+    for(int i=lessLength;i>0;i--){
+        if(c[len+i]!=0){
+            CLength+=i;
+            break;
+        }
+    }
+
+    for(int i=CLength;i>=1;i--){
+        cout << c[i];
+    }
+    return 0;
+}
---
+++ LuoGu/P1601.cpp
@@ -0,0 +1,80 @@
+//
+// Created by Administrator on 2025/5/29.
+//
+//#include "iostream"
+//#include "string"
+////#include "algorithm"
+//using namespace std;
+//// 若需定义常量，推荐const int maxn = 999; 这是预处理指令，在编译前进行文本替换代码中所有maxn会被直接替换为999
+//// 且要注意后面不能带;
+//#define maxn 999
+//int add1[maxn],add2[maxn];
+//int main(){
+//    // 分别获取
+//    string add1Str,add2Str;
+//    cin >> add1Str >> add2Str;
+//    int add1StrLen = add1Str.length();
+//    int add2StrLen = add2Str.length();
+//    // 给短的那位前面补0
+//    int maxStrLen = max(add1StrLen , add2StrLen);
+//    if(maxStrLen > add2StrLen){
+//        add2Str = std::string(add1StrLen - add2StrLen, '0') + add2Str;
+//    } else {
+//        add1Str = std::string(add2StrLen - add1StrLen, '0') + add1Str;
+//    }
+//    // 从最后面开始，每位相加，满十进1
+//    int finalArray[maxStrLen];
+//    bool haveJinWei = false;
+//    for (int i = maxStrLen-1; i >=0; --i) {
+//        finalArray[maxStrLen-1 -i] = (add1Str[i]-'0') + (add2Str[i]-'0') + haveJinWei;
+//        if(finalArray[maxStrLen-1 -i] >= 10){
+//            haveJinWei = true;
+//            finalArray[maxStrLen-1 -i] = finalArray[maxStrLen-1 -i]-10;
+//        }else{
+//            haveJinWei = false;
+//        }
+//    }
+//
+//    // 遍历反过来，如果最后一位没有满十就不输出最后一位
+//    if(haveJinWei){
+//        cout << 1;
+//    }
+//    for(int i = 0;i<maxStrLen;i++){
+//        cout << finalArray[i];
+//    }
+//
+//    return 0;
+//}
+
+// 碰到输入长串数字并逐位处理的，需要考虑到string获取并将它们拆分放到数组内去，好一位位处理。
+#include "iostream"
+#include "string"
+#include "algorithm"
+#define maxn 520
+using namespace std;
+int a[maxn],b[maxn],c[maxn];
+int main(){
+    string A,B;
+    cin >> A >> B;
+    int len = max(A.length(),B.length());
+    // 反过来获取了
+    for(int i = A.length()-1,j=1;i>=0;i--,j++){
+        a[j] = A[i]-'0';
+    }
+    for(int i = B.length()-1,j=1;i>=0;i--,j++){
+        b[j] = B[i]-'0';
+    }
+    for(int i=1;i<=len;i++){
+        c[i] += a[i] + b[i];    // 此处用的是 "+=" 而是不 “=”，所以上一次的进位在这里也有
+        cout << "now:" << a[i] << ";" << b[i] <<";" << c[i] <<endl;
+        c[i+1] = c[i]   / 10;
+        c[i] %= 10;
+    }
+    if(c[len+1]){
+        len++;
+    }
+    for(int i=len;i>=1;i--){
+        cout << c[i];
+    }
+    return 0;
+};
